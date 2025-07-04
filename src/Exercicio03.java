d8387f00bc4a9a81b9ba22f2ee2552cb212d92f2
Julia <juliaborgesmatilde@gmail.com>
2025-07-04 19:54:17 -0300

    Exercicio 03

--- src/Exercicio03.java
+++ src/Exercicio03.java
@@ -1,13 +1,5 @@
 import java.util.Scanner;
 
-/*
-* A tarifa do transporte público depende dessas condições:
-Se tiver menos de 6 anos ou mais de 65 → não paga nada
-Se for estudante de até 25 anos → paga 50% da tarifa
-Todos os outros pagam tarifa cheia (R$ 4,00)
-Mostre a mensagem:
-Valor a pagar: R$ X
-*/
 public class Exercicio03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
